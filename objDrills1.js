cf306c6cedc6a51e8289c8899fccb189c150f61b
Darren <ninenumbproductions@gmail.com>
2018-04-04 08:42:11 -0700

    self reference drill

--- objDrills1.js
+++ objDrills1.js
@@ -1,25 +1,39 @@
 'use strict';
 
-function createMyObject() {
-  return {
-    foo: 'bar',
-    answerToUniverse: 42,
-    'olly olly': 'oxen free',
-    sayHello: function(){
-      return 'hello';
+// function createMyObject() {
+//   return {
+//     foo: 'bar',
+//     answerToUniverse: 42,
+//     'olly olly': 'oxen free',
+//     sayHello: function(){
+//       return 'hello';
+//     },
+//   };
+// }
+
+// let monkey ={};
+
+// function updateObject(obj) {
+//   obj.foo = 'foo';
+//   obj.bar = 'bar';
+//   obj.bizz = 'bizz';
+//   obj.bang = 'bang';
+
+//   return obj;
+// }
+
+// console.log(updateObject(monkey));
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){
+      return `${this.firstName} ${this.lastName}`;
     },
   };
+  return person;
 }
 
-let monkey ={};
-
-function updateObject(obj) {
-  obj.foo = 'foo';
-  obj.bar = 'bar';
-  obj.bizz = 'bizz';
-  obj.bang = 'bang';
-
-  return obj;
-}
-
-console.log(updateObject(monkey));
