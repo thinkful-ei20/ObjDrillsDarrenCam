d8d396f8621c36f53dea9cc4aa17fc087e50879c
Darren <ninenumbproductions@gmail.com>
2018-04-04 08:55:13 -0700

    studentreport drill

--- objDrills1.js
+++ objDrills1.js
@@ -49,4 +49,16 @@ function keyDeleter(obj){
   return obj;
 }
 
-console.log(keyDeleter(objTest));
+// console.log(keyDeleter(objTest));
+
+function makeStudentsReport(data) {
+  return data.map(student => `${student.name}: ${student.grade}`); 
+}
+
+const testData = [
+  { name: 'Jane Doe', grade: 'A' },
+  { name: 'John Dough', grade: 'B' },
+  { name: 'Jill Do', grade: 'A' },
+];
+
+// console.log(makeStudentsReport(testData));
