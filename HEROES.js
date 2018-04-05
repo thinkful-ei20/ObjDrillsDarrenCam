dd0f4e3f02da67b2d65bd590d3147f156b806901
Cam Prier <CSPrier@gmail.com>
2018-04-04 17:19:49 -0700

    DATABASE HEROES Bonnus #1

---
+++ HEROES.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function matches(hero, query) {
+  if (hero.id === query.id) {
+    return true;
+  }
+}
+
+function findOne(arr, query) {
+  // returns hero that matches query number
+  // return arr.find(hero => hero.id === query ? hero : null);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (matches(arr[i], query)) {
+      return arr[i];
+    }
+  }
+  return null;
+}
+
+console.log(findOne(HEROES, { id: 1 })); // { id: 1, name: 'Captain America', squad: 'Avengers' }
+console.log(findOne(HEROES, { id: 10 })); // null
+console.log(findOne(HEROES, { id: 2, name: 'Aquaman' })); // null
+console.log(findOne(HEROES, { id: 5, squad: 'Justice League' })); //  { id: 5, name: 'Wonder Woman', squad: 'Justice League' }
+// fineOne(HEROES, { squad: 'Justice League' }); // { id: 4, name: 'Superman', squad: 'Justice League' }
